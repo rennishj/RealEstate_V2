4621d5b263819ce063030b1a3d6bca980aade434
rennishj <rennishj@gmail.com>
2016-08-29 15:47:51 -0400

    Initial check in of RealEstate

---
+++ RealEstate.DAL/ConnectionHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Insight.Database;
+using System.Data;
+using System.Configuration;
+
+namespace RealEstate.DAL
+{
+    public class ConnectionHelper
+    {
+        public static IDbConnection ConnString
+        {
+            get { return ConfigurationManager.ConnectionStrings["RealEstate"].Connection(); }
+        }
+    }
+}
---
+++ RealEstate.Poco/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Poco
+{
+   public class Address
+    {
+        public int AddressId { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Zip { get; set; }
+        public string PostOffice { get; set; }
+    }
+}
